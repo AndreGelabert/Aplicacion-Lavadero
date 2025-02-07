710aece16b4ac3427f0ccfaf2e04ad01d14dda55
André Gelabert <andregelabert21@gmail.com>
2025-02-07 15:51:32 -0300

    Implementacion de una barra de busqueda sencilla en la vista de Personal

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -64,3 +64,24 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 });
+function filterTable() {
+    var input, filter, table, tr, td, i, j, txtValue;
+    input = document.getElementById("simple-search");
+    filter = input.value.toUpperCase();
+    table = document.querySelector("table");
+    tr = table.getElementsByTagName("tr");
+
+    for (i = 1; i < tr.length; i++) {
+        tr[i].style.display = "none";
+        td = tr[i].getElementsByTagName("td");
+        for (j = 0; j < td.length; j++) {
+            if (td[j]) {
+                txtValue = td[j].textContent || td[j].innerText;
+                if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                    tr[i].style.display = "";
+                    break;
+                }
+            }
+        }
+    }
+}
